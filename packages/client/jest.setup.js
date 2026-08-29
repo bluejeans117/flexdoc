4b8a256c90ad205686faa58c13a9e32b0d469c21
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-08-30 02:22:19 +0530

    fix(client): make Jest setup valid ESM

--- packages/client/jest.setup.js
+++ packages/client/jest.setup.js
@@ -1,10 +1,7 @@
-// Import jest-dom matchers
-require('@testing-library/jest-dom');
+import '@testing-library/jest-dom';
 
-// Mock for ResizeObserver
 global.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
   disconnect() {}
 };
-
