9cfdc389c3fa19f343376489562b184b5a10374c
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-08-30 03:19:22 +0530

    feat(backend): export framework adapter helpers

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,3 +2,5 @@ export { FlexDocModule } from './flexdoc.module';
 export { FlexDocService } from './flexdoc.service';
 export { FlexDocOptions, FlexDocModuleOptions } from './interfaces';
 export { setupFlexDoc } from './setup';
+export { setupExpressFlexDoc, setupFastifyFlexDoc } from './framework-adapters';
+export type { ExpressLikeApplication, FastifyLikeApplication, FastifyLikeReply, FastifyLikeRequest } from './framework-adapters';
