99dd3a174de58b008ee8928f79afac915cbaf29b
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-08-30 02:24:36 +0530

    fix(client): use CommonJS Jest config under ESM package

---
+++ packages/client/jest.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.cjs',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.cjs',
+    '^prismjs$': '<rootDir>/__mocks__/prismjs.cjs',
+    '^prismjs/components/.*$': '<rootDir>/__mocks__/prismjs.cjs',
+    '^prismjs/themes/.*$': '<rootDir>/__mocks__/styleMock.cjs',
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.cjs',
+    '<rootDir>/src/setupTests.ts',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/*.{spec,test}.{ts,tsx}'],
+};
