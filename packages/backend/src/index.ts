086d6b81b74be268a702332898944706971124da
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-08-31 15:10:58 +0530

    Export Fastify Swagger integration

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,5 +2,5 @@ export { FlexDocModule } from './flexdoc.module';
 export { FlexDocService } from './flexdoc.service';
 export { FlexDocOptions, FlexDocModuleOptions } from './interfaces';
 export { setupFlexDoc } from './setup';
-export { setupExpressFlexDoc, setupFastifyFlexDoc, setupNestFlexDoc } from './framework-adapters';
+export { setupExpressFlexDoc, setupFastifyFlexDoc, setupFastifySwaggerFlexDoc, setupNestFlexDoc } from './framework-adapters';
 export type { ExpressLikeApplication, FastifyLikeApplication, FastifyLikeReply, FastifyLikeRequest, NestLikeApplication } from './framework-adapters';
