e402430e8ae7f0476809864d997ba7a57bede7a4
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-09-03 17:34:48 +0530

    Add framework-neutral Python host and WSGI support

--- adapters/python/src/prauga_flexdoc/__init__.py
+++ adapters/python/src/prauga_flexdoc/__init__.py
@@ -1,4 +1,15 @@
-from .asgi import FlexDocASGI, FlexDocConfig
-from .integrations import setup_fastapi_flexdoc
+from .host import FlexDocConfig, FlexDocHost, FlexDocResponse
+from .asgi import FlexDocASGI
+from .wsgi import FlexDocWSGI
+from .integrations import django_urlpatterns, setup_fastapi_flexdoc, setup_flask_flexdoc
 
-__all__ = ["FlexDocASGI", "FlexDocConfig", "setup_fastapi_flexdoc"]
+__all__ = [
+    "FlexDocASGI",
+    "FlexDocConfig",
+    "FlexDocHost",
+    "FlexDocResponse",
+    "FlexDocWSGI",
+    "django_urlpatterns",
+    "setup_fastapi_flexdoc",
+    "setup_flask_flexdoc",
+]
